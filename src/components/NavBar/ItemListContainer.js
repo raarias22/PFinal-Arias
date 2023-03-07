bdcb51c87b799c50439a5566f207263fcd366e63
Rodrigo <rodrigoariasramirez@gmail.com>
2023-03-07 07:57:18 -0300

    agrego yarn

--- src/components/NavBar/ItemListContainer.js
+++ src/components/NavBar/ItemListContainer.js
@@ -1,11 +1,14 @@
 import React from "react";
+import Title from '../Title/Title'
 
-const ItemListContainer = () => {
+
+export const ItemListContainer = ({ texto }) => {
     return (
-        <div>
+        <>
 
+            <Title greeting= {texto}/>
 
-        </div>
+        </>
 
 
     )
