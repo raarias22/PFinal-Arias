eb57d86fd63db12804aa3fdeaad1acdfbcb3cb84
Rodrigo <rodrigoariasramirez@gmail.com>
2023-03-24 18:59:46 -0300

    modifico descripción itemdetail

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Routes>
               <Route path={"/"} element={<ItemListContainer />} />
               <Route path={"/category/:id"} element={<ItemListContainer />} />
-              <Route path={"/item/id"} element={<ItemDetailContainer />} />
+              <Route path={"/item/:id"} element={<ItemDetailContainer />} />
               <Route path={"*"} element={<Error404 />} />
             </Routes>           
           </BrowserRouter>
